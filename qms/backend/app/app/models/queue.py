6f46f0f69eb95ae1f2d08f30dee42f4770443f74
Kenneth Aalberg <kenneaal@gmail.com>
2021-05-17 20:06:23 +0200

    Make arrival_time tzaware.

--- qms/backend/app/app/models/queue.py
+++ qms/backend/app/app/models/queue.py
@@ -13,7 +13,7 @@ class Queue(Base):
     id = Column(Integer, primary_key=True, index=True)
     client_id = Column(Integer, ForeignKey("client.id"))
     client = relationship("Client", back_populates='queue')
-    arrival_time = Column(DateTime)
+    arrival_time = Column(DateTime(timezone=True))
     pending = Column(Boolean)
     in_progress = Column(Boolean)
     uuid = Column(String)
