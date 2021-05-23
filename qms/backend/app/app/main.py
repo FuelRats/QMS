c06afe17cc88386788362efbd8d283d1e26db5b5
Kenneth Aalberg <kenneaal@gmail.com>
2021-05-23 11:41:12 +0200

    Add 10 minute timeout on queue items that are pending but have not gone in_progress.

--- qms/backend/app/app/main.py
+++ qms/backend/app/app/main.py
@@ -1,8 +1,13 @@
+import datetime
+
 from fastapi import FastAPI, Depends
+from fastapi_utils.tasks import repeat_every
 from starlette.middleware.cors import CORSMiddleware
 
 from app.api.api_v1.api import api_router
 from app.core.config import settings
+from app.api import deps
+from app.models.queue import Queue
 
 from fastapi.security import OAuth2PasswordBearer
 
@@ -11,6 +16,20 @@ app = FastAPI(
 )
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/login/access-token")
+
+
+@app.on_event("startup")
+@repeat_every(seconds=60)
+def cleanup_old_queue() -> None:
+    db = Depends(deps.get_db)
+    timeout = datetime.datetime.utcnow() - datetime.timedelta(minutes=15)
+    old_queue = db.query(Queue).filter(Queue.pending == True).\
+        filter(Queue.arrival_time <= timeout).filter(Queue.in_progress == False)
+    for row in old_queue:
+        print(f"Timing out queue entry {row.uuid}")
+        row.delete()
+    db.commit()
+
 
 # Set all CORS enabled origins
 if settings.BACKEND_CORS_ORIGINS:
