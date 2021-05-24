50694aec68dd3cb3a2ba15506e748d425d983da0
Kenneth Aalberg <kenneaal@gmail.com>
2021-05-24 20:17:49 +0200

    Just pull localsession directly from the app session, see if that works...

--- qms/backend/app/app/worker.py
+++ qms/backend/app/app/worker.py
@@ -10,14 +10,6 @@ from app.core.config import settings
 client_sentry = Client(settings.SENTRY_DSN)
 
 
-def get_session():
-    try:
-        db = SessionLocal()
-        yield db
-    finally:
-        db.close()
-
-
 @celery_app.task(acks_late=True)
 def test_celery(word: str) -> str:
     return f"test task return {word}"
@@ -25,7 +17,7 @@ def test_celery(word: str) -> str:
 
 @celery_app.task(acks_late=True)
 def clean_queue(msg: str) -> str:
-    db = get_session()
+    db = SessionLocal()
     print("In clear_queue()")
     timeout = datetime.datetime.utcnow() - datetime.timedelta(minutes=15)
     old_queue = db.query(Queue).filter(Queue.pending == True).\
