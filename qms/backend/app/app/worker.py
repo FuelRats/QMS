c28921ad0943009b17674480551c37397030f23b
Kenneth Aalberg <kenneaal@gmail.com>
2021-05-24 20:14:39 +0200

    Try local session for celeryworker...

--- qms/backend/app/app/worker.py
+++ qms/backend/app/app/worker.py
@@ -1,15 +1,21 @@
 import datetime
 
-from app.api import deps
 from app.models import Queue
-from fastapi import Depends
 from raven import Client
 
+from app.db.session import SessionLocal
 from app.core.celery_app import celery_app
 from app.core.config import settings
-from sqlalchemy.orm import Session
 
 client_sentry = Client(settings.SENTRY_DSN)
+
+
+def get_session():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
 
 
 @celery_app.task(acks_late=True)
@@ -19,7 +25,7 @@ def test_celery(word: str) -> str:
 
 @celery_app.task(acks_late=True)
 def clean_queue(msg: str) -> str:
-    db: Session = Depends(deps.get_db)
+    db = get_session()
     print("In clear_queue()")
     timeout = datetime.datetime.utcnow() - datetime.timedelta(minutes=15)
     old_queue = db.query(Queue).filter(Queue.pending == True).\
