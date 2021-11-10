8c44c982c0fcb47dbf6b3733ffe50a82f8b290ff
Cameron Welter <camjwelt@gmail.com>
2021-11-10 17:17:44 -0500

    no more btoa

--- qms/frontend/src/helpers/PushClientToKiwi.ts
+++ qms/frontend/src/helpers/PushClientToKiwi.ts
@@ -9,5 +9,5 @@ export default function pushClientToKiwi(prefilledData: {
   window.location.href =
     process.env.NEXT_PUBLIC_KIWI_URL +
     "?prefilledData=" +
-    btoa(JSON.stringify(prefilledData));
+    encodeURIComponent(JSON.stringify(prefilledData));
 }
