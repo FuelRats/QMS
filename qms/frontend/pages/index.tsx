c36f4ccdd4ea343d725dd77f57c584d4ce8de160
Delota <79107184+Delota@users.noreply.github.com>
2022-02-16 22:49:19 +0100

    Include timestamp in pushClientToKiwi hotfix

--- qms/frontend/pages/index.tsx
+++ qms/frontend/pages/index.tsx
@@ -173,6 +173,7 @@ export default function Home() {
       timer: codeRed === EmptyBoolean.TRUE,
       odyssey: odyssey === EmptyBoolean.TRUE,
       submit: true,
+      time: Date.now(),
     });
   }
 
