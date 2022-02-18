f8b86f8656be9f06fade91cd127c19a330b20582
Delota <smissot+delota@gmail.com>
2022-02-18 23:36:16 +0100

    Prevent NextJS from exiting on fatal error

--- qms/frontend/sentry.server.config.js
+++ qms/frontend/sentry.server.config.js
@@ -8,5 +8,8 @@ if (process.env.SENTRY_SERVER_DSN) {
     // `release` value here - use the environment variable `SENTRY_RELEASE`, so
     // that it will also get attached to your source maps
     environment: process.env.NEXTJS_ENVIRONMENT,
+    // Without this, sentry forces the shutdown, while the default behaviour does not
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    onFatalError() {},
   });
 }
